d62d3f4eed504c4eee03bef008686481aa59ad32
bumslang <bumslang8@gmail.com>
2022-04-01 19:53:16 +0300

    change days

--- src/Days/Days.jsx
+++ src/Days/Days.jsx
@@ -1,29 +1,30 @@
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 import './Days.css'
 
 
-function Days({ nowYear, nowMonth, firstWeekDays, setFullDate, }) {
+function Days({ nowYear, nowMonth, firstWeekDays,  setFullDate,}) {
     function createDays(numOfDayss) {
         let days = [];
-        let columns = [[<p key={nanoid()} className="column__weekday" >Понедельник</p>], [<p key={nanoid()} className="column__weekday" >Вторник</p>], [<p key={nanoid()} className="column__weekday" >Среда</p>], [<p key={nanoid()} className="column__weekday" >Четверг</p>], [<p key={nanoid()} className="column__weekday" >Пятница</p>], [<p key={nanoid()} className="column__weekday" >Суббота</p>], [<p key={nanoid()} className="column__weekday" >Воскресенье</p>]];
+        let columns = [[<p key={nanoid()}  className="column__weekday" >Понедельник</p>], [<p key={nanoid()}  className="column__weekday" >Вторник</p>], [<p key={nanoid()}  className="column__weekday" >Среда</p>], [<p key={nanoid()}  className="column__weekday" >Четверг</p>], [<p key={nanoid()}  className="column__weekday" >Пятница</p>], [<p key={nanoid()}  className="column__weekday" >Суббота</p>],[<p key={nanoid()}  className="column__weekday" >Воскресенье</p>]];
         if (firstWeekDays === 0) firstWeekDays = 7;
         for (let i = 0, k = 0; k < numOfDayss; i++) {
             if (i < firstWeekDays - 1) {
-                days[i] = <p className="column__item column__item_hidden" key={nanoid()}><span>.</span></p>;
+                days[i] = <p className="column__item column__item_hidden"  key={nanoid()}><span>.</span></p>;
             } else {
                 k++;
-                days[i] = <p className='column__item' onClick={(e) => setCurrentDate(e.target.innerHTML)} key={nanoid()}>{k}</p>;
+                days[i] = <p className='column__item'  onClick={(e) => setCurrentDate(e.target.innerHTML)} key={nanoid()}>{k}</p>;
             }
         }
 
         function setCurrentDate(elem) {
             setFullDate('' + elem + nowMonth + nowYear);
-
+         
         }
 
         days.forEach((elem, ind) => {
             for (let i = 0; i < 7; i++) {
-                if (ind % 7 === i) columns[i].push(elem);
+                if (ind%7===i) columns[i].push(elem)
             }
         });
 
